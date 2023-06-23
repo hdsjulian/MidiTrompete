b000f51645340796397928ef8887e6733dcc8fed
Julian <julian@phinn.de>
2023-06-23 23:53:47 +0200

    Update main.cpp
    fixed

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #define POTI_BOTTOM A1
 #define BUTTONS_FRONT 1
 #define BUTTONS_BACK 1
-#define POTIS_MIDDLE 1 
+#define POTIS 1
 
 const int buttonFrontTop = BUTTON_FRONT_TOP;
 const int buttonFrontBottom = BUTTON_FRONT_BOTTOM;
@@ -23,84 +23,85 @@ const int potiTop = POTI_TOP;
 const int potiBottom = POTI_BOTTOM;
 
 const uint8_t midi_channel = 1;
+const uint8_t controller_number = 0;
 
 int midicommands_ft[][2] = {{16, 17}, {18, 19}, {20, 21}, {22, 23}};
-25,;26,27};
+
+int midicommands_fb[] = {24, 25, 26, 27};
 int channel_ft = 0;
-int channel_bb = 0;
+int channel_fb = 0;
 
-static int averageValue_pt = 0;
-static int averageValue_pb = 0;
-static int hysteresis_pt = 0;
-static int hysteresis_pb = 0;
+static int averageValue_top = 0;
+static int averageValue_bottom = 0;
+static int hysteresis_top = 0;
+static int hysteresis_bottom = 0;
 
-
-ftid click_ft() {
+void click_ft() {
   BLEMidiServer.controlChange(midi_channel, midicommands_ft[channel_ft][0], 100);
   delay(100);
   BLEMidiServer.controlChange(midi_channel, midicommands_ft[channel_ft][0], 0);
   //Serial.println("VO");
 }
-ftid longclick_ft() {
+void longclick_ft() {
   BLEMidiServer.controlChange(midi_channel, midicommands_ft[channel_ft][1], 100);
   delay(100);
     BLEMidiServer.controlChange(midi_channel, midicommands_ft[channel_ft][1], 0);
     //Serial.println("VO");
 }
-ftid click_bb() {
-  BLEMidiServer.controlChange(midi_channel, midicommands_bb[channel_bb], 127);
+void click_fb() {
+  BLEMidiServer.controlChange(midi_channel, midicommands_fb[channel_fb], 127);
 
 }
-ftid longclick_bb() {
-    BLEMidiServer.controlChange(midi_channel, midicommands_bb[channel_bb], 0);
+void longclick_fb() {
+    BLEMidiServer.controlChange(midi_channel, midicommands_fb[channel_fb], 0);
     //Serial.println("VO");
 }
-ftid doubleclick_ft() {
+void doubleclick_ft() {
   if (channel_ft <3) {
     channel_ft++;
     BLEMidiServer.noteOn(0, 60, 100+channel_ft);
   }
 }
-ftid doubleclick_bt() {
+void doubleclick_bt() {
   if (channel_ft > 0) {
     channel_ft--;
     BLEMidiServer.noteOn(0, 60, 100+channel_ft);
   }
 }
 
-ftid doubleclick_bb() {
-  if (channel_bb <3) {
-    channel_bb++;
-    BLEMidiServer.noteOn(0, 61, 100+channel_bb);
+void doubleclick_fb() {
+  if (channel_fb <3) {
+    channel_fb++;
+    BLEMidiServer.noteOn(0, 61, 100+channel_fb);
   }
 }
-ftid doubleclick_bt() {
-  if (channel_bb > 0) {
-    channel_bb--;
-    BLEMidiServer.noteOn(0, 61, 100+channel_bb);
+void doubleclick_bb() {
+  if (channel_fb > 0) {
+    channel_fb--;
+    BLEMidiServer.noteOn(0, 61, 100+channel_fb);
   }
 }
 
-ftid click_bt() {
+void click_bt() {
   BLEMidiServer.controlChange(midi_channel, 28, 100);
   delay(100);
   BLEMidiServer.controlChange(midi_channel, 28, 0);
 //Serial.println("HU");
 }
-ftid longclick_bt() {
+void longclick_bt() {
   BLEMidiServer.controlChange(midi_channel, 29, 100);
   delay(100);
     BLEMidiServer.controlChange(midi_channel, 29, 0);
     //Serial.println("VO");
 }
 
-ftid click_bt() {
+void click_bb() {
   BLEMidiServer.controlChange(midi_channel, 30, 100);
   delay(100);
   BLEMidiServer.controlChange(midi_channel, 30, 0);
 //Serial.println("HU");
 }
-ftid longclick_bt() {
+void longclick_bb() {
   BLEMidiServer.controlChange(midi_channel, 31, 100);
   delay(100);
     BLEMidiServer.controlChange(midi_channel, 31, 0);
@@ -108,27 +109,27 @@ ftid longclick_bt() {
 }
 
 
-ftid setup() {
+void setup() {
   //Serial.begin(115200);
   //Serial.println("Initializing bluetooth");
   BLEMidiServer.begin("Basic MIDI device");
   //Serial.println("Waiting for connections...");
   BLEMidiServer.enableDebugging();  // Uncomment if you want to see some debugging output from the library
   ButtonFrontTop.attachClick(click_ft);
-  ButtonFrontBottom.attachClick(click_bb);
+  ButtonFrontBottom.attachClick(click_fb);
   ButtonBackTop.attachClick(click_bt);
-  ButtonBackBottom.attachClick(click_bt);
+  ButtonBackBottom.attachClick(click_bb);
   ButtonFrontTop.attachLongPressStart(longclick_ft);
-  ButtonFrontBottom.attachLongPressStart(longclick_bb);
+  ButtonFrontBottom.attachLongPressStart(longclick_fb);
   ButtonBackTop.attachLongPressStart(longclick_bt);
-  ButtonBackBottom.attachLongPressStart(longclick_bt);
+  ButtonBackBottom.attachLongPressStart(longclick_bb);
   ButtonFrontTop.attachDoubleClick(doubleclick_ft);
-  ButtonFrontBottom.attachDoubleClick(doubleclick_bb);
+  ButtonFrontBottom.attachDoubleClick(doubleclick_fb);
   ButtonBackTop.attachDoubleClick(doubleclick_bt);
-  ButtonBackBottom.attachDoubleClick(doubleclick_bt);
+  ButtonBackBottom.attachDoubleClick(doubleclick_bb);
 }
 
-ftid loop() {
+void loop() {
   if(BLEMidiServer.isConnected()) {             // If we've got a connection, we send an A4 during one second, at full velocity (127)
   if (BUTTONS_FRONT == 1) {
     ButtonFrontTop.tick();
@@ -138,19 +139,22 @@ ftid loop() {
     ButtonBackTop.tick();
     ButtonBackBottom.tick();
   }
-  if (POTIS_MIDDLE == 1) { 
-    int sensorValue_pt = analogRead(potiTop)/32;
-    averageValue_pt = (averageValue_pt * 7 + sensorValue_pt) / 8;  //cheap lowpass
-    if (abs(hysteresis_pt-averageValue_pt) > 8){
-      hysteresis_pt = averageValue_pt;
-      BLEMidiServer.controlChange(midi_channel, 11, hysteresis_pt);
+  if (POTIS == 1) { 
+    int sensorValue_top = analogRead(potiTop)/32;
+    averageValue_top = (averageValue_top * 7 + sensorValue_top) / 8;  // billig Tiefpass um ausreißer wegzufiltern
+    if (abs(hysteresis_top-averageValue_top) > 8){
+      hysteresis_top = averageValue_top;
+      Serial.println(hysteresis_top);
+      BLEMidiServer.controlChange(midi_channel, 12, hysteresis_top);
     }
-    int sensorValue_pb = analogRead(potiBottom)/32;
-    averageValue_pb = (averageValue_pt * 7 + sensorValue_pb) / 8;  //cheap lowpass
-    if (abs(hysteresis_pb-averageValue_pb) > 8){
-      hysteresis_pb = averageValue_pb;
-      BLEMidiServer.controlChange(midi_channel, 12, hysteresis_pb);
+    int sensorValue_bottom = analogRead(potiBottom)/32;
+    averageValue_bottom = (averageValue_bottom * 7 + sensorValue_bottom) / 8;  // billig Tiefpass um ausreißer wegzufiltern
+    if (abs(hysteresis_bottom-averageValue_bottom) > 8){
+      hysteresis_bottom = averageValue_bottom;
+      Serial.println(hysteresis_bottom);
+      BLEMidiServer.controlChange(midi_channel, 12, hysteresis_bottom);
     }
   }
+  delay(10);
   }
 }
